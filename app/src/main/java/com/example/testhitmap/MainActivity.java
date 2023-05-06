1c0c60808f3d0ac35ba4f3be36daf2579bb858ab
n0Hu <tarzan0407ser@mail.ru>
2023-05-07 00:30:02 +0300

    Рефакторинг MainActivity + Фикс работы с БД

--- app/src/main/java/com/example/testhitmap/MainActivity.java
+++ app/src/main/java/com/example/testhitmap/MainActivity.java
@@ -1,10 +1,18 @@
 package com.example.testhitmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.accessibilityservice.AccessibilityService;
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
+import android.os.Debug;
 import android.provider.Settings;
+import android.text.TextUtils;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -14,64 +22,68 @@ import android.os.Environment;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Locale;
+
 import android.database.sqlite.SQLiteDatabase;
 import android.database.Cursor;
 
 
 public class MainActivity extends AppCompatActivity {
-    public Button button;
     MyDatabaseHelper DBHelper;
+
+    private Intent serviceLogs;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        button = findViewById(R.id.button);
-        // Запускаем сервис для получения слепка активности пользователя
-        startService(new Intent(this, MyService.class));
+        if (!isAccessibilityServiceEnabled(this, MyAccessibilityService.class)) {
+            startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
+        }
 
+        this.serviceLogs = new Intent(this, MyService.class);
 
-
-        button.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(Settings.ACTION_ACCESSIBILITY_SETTINGS));
-
-            }
-        });
+        this.DBHelper = new MyDatabaseHelper(getApplicationContext());
     }
 
+    public void startServiceLog(View v) {
+        // Запускаем сервис для получения слепка активности пользователя
+        startService(this.serviceLogs);
+    }
 
+    public void stopServiceLog(View v) {
+        stopService(this.serviceLogs);
+    }
 
 
 
     public void save(View v) {
         Date currentDate = new Date();
-        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss");
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd_HH-mm-ss", Locale.UK);
         String date = dateFormat.format(currentDate);
         SQLiteDatabase database = DBHelper.getWritableDatabase();
         String fileName = date + "log.txt";
         String directoryPath = Environment.getExternalStorageDirectory().getAbsolutePath() + "/Documents/";
         String filePath = directoryPath + fileName;
 
-        Cursor cursor = database.query(DBHelper.TABLE_LOGS, null, null, null, null, null, null);
+        Cursor cursor = database.query(MyDatabaseHelper.TABLE_LOGS, null, null, null, null, null, null);
 
         StringBuilder dataBuilder = new StringBuilder();
 
         while (cursor.moveToNext()) {
 
-            int idIndex = cursor.getColumnIndex(DBHelper.KEY_ID);
-            int XIndex = cursor.getColumnIndex(DBHelper.KEY_X);
-            int YIndex = cursor.getColumnIndex(DBHelper.KEY_Y);
-            int namepIndex = cursor.getColumnIndex(DBHelper.KEY_NAMEPAGE);
-            int nameeIndex = cursor.getColumnIndex(DBHelper.KEY_NAMEELEMENT);
+            int idIndex = cursor.getColumnIndex(MyDatabaseHelper.KEY_ID);
+            int XIndex = cursor.getColumnIndex(MyDatabaseHelper.KEY_X);
+            int YIndex = cursor.getColumnIndex(MyDatabaseHelper.KEY_Y);
+            int namepIndex = cursor.getColumnIndex(MyDatabaseHelper.KEY_NAMEPAGE);
+            int nameeIndex = cursor.getColumnIndex(MyDatabaseHelper.KEY_NAMEELEMENT);
 
             int idValue = cursor.getInt(idIndex);
             String XValue = cursor.getString(XIndex);
             int YValue = cursor.getInt(YIndex);
 
-            dataBuilder.append(String.format("ID= %d, NAME= %s, PASSWORD= %d \n", idValue, XValue, YValue, namepIndex, nameeIndex));
+            dataBuilder.append(String.format(Locale.UK, "ID= %d, NAME= %s, PASSWORD= %d \n", idValue, XValue, YValue, namepIndex, nameeIndex));
         }
 
         cursor.close();
@@ -97,8 +109,36 @@ public class MainActivity extends AppCompatActivity {
 
     public void clear(View v) {
         SQLiteDatabase database = DBHelper.getWritableDatabase();
-        database.delete(DBHelper.TABLE_LOGS, null, null);
+        database.delete(MyDatabaseHelper.TABLE_LOGS, null, null);
+    }
 
+    private boolean isAccessibilityServiceEnabled(Context context, Class<? extends AccessibilityService> accessibilityServiceClass) {
+        int accessibilityEnabled = 0;
+        final String serviceId = context.getPackageName() + "/" + accessibilityServiceClass.getCanonicalName();
+        try {
+            accessibilityEnabled = Settings.Secure.getInt(
+                    context.getApplicationContext().getContentResolver(),
+                    android.provider.Settings.Secure.ACCESSIBILITY_ENABLED);
+        } catch (Settings.SettingNotFoundException e) {
+            // Ошибка при получении значения. Используется значение по умолчанию
+        }
+        TextUtils.SimpleStringSplitter colonSplitter = new TextUtils.SimpleStringSplitter(':');
 
+        if (accessibilityEnabled == 1) {
+            String settingValue = Settings.Secure.getString(
+                    context.getApplicationContext().getContentResolver(),
+                    Settings.Secure.ENABLED_ACCESSIBILITY_SERVICES);
+            if (settingValue != null) {
+                colonSplitter.setString(settingValue);
+                while (colonSplitter.hasNext()) {
+                    String accessibilityService = colonSplitter.next();
+
+                    if (accessibilityService.equalsIgnoreCase(serviceId)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
     }
 }
